010805e4986dd33ddc4ff4611ef2fe874d2803a4
enes054 <155081800+enes054@users.noreply.github.com>
2024-12-03 23:34:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sayfa yüklendiğinde çalışacak fonksiyon
+document.addEventListener('DOMContentLoaded', function() {
+    // Hoş geldiniz mesajını göster
+    setTimeout(function() {
+        alert("Yedupa'ya Hoş Geldiniz!");
+    }, 500);
+}); 
